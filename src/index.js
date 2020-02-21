45b3b1cb4b5f1552187f178deafabcf463458963
chewcw <chwai87@gmail.com>
2020-02-21 10:35:13 +0800

    remove netplan and dhcpd implementation

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 'use strict'
 
 var interfaces_d = require('./interfaces.d/index.js')
-var dhcpcd = require('./dhcpcd/index.js')
-var netplan = require('./netplan/index.js')
 var { exec } = require('child_process')
 var { promisify } = require('util')
 var execPromise = promisify(exec)
@@ -27,33 +25,16 @@ exports.configure = async (configs) => {
     return a.vlanid && !b.vlanid ? 1 : 0;
   })
   await Promise.all([
-    dhcpcd.configure(configs),
     interfaces_d.configure(configs),
-    netplan.configure(configs)
   ])
 
 }
 
 exports.restartService = async () => {
-  var error
-  var network_service_restarted = false
-  var networking_restart = execPromise('service networking restart')
-    .then(() => network_service_restarted = true)
-    .catch(e => {
-      console.log(e)
-      error = e
-    })
-  var netplan_restart = execPromise('netplan try')
-    .then(() => execPromise('netplan apply'))
-    .then(() => network_service_restarted = true)
-    .catch(e => {
-      console.log(e)
-      error = e
-    })
-
-  await Promise.all([networking_restart, netplan_restart])
-
-  if (!network_service_restarted)
-    return Promise.reject(error)
-
+  try {
+    await execPromise('service networking restart')
+  } catch (err) {
+    console.log(err)
+    return Promise.reject(err)
+  }
 }
